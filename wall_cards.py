324ce28824af4667b3955f7578525dc02359f995
Mmd <mohammad.r.nemati03@gmail.com>
2022-03-26 17:23:58 +0430

    Modify a typo in wall_cards.py

--- wall_cards.py
+++ wall_cards.py
@@ -1,17 +1,14 @@
 import card
-import clues
+import clue
 
-wall_cards = []
+all_wall_cards = []
 
 class WallCard(card.Card):
     
     def __init__(self, given_clues):
         self.wall_clues = []
-        global wall_cards
+        global all_wall_cards
         for i in given_clues:
-            locals()[i] = clues.Clue(i)
+            locals()[i] = clue.Clue(i)
             self.wall_clues.append(locals()[i])
-        wall_cards.append(self)
-
-a = WallCard(["woman", "vassal", "red_hair", "strong"])
-print(a)
+        all_wall_cards.append(self)
